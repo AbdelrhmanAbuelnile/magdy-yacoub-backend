00c5f5b250a49ecb3b5fbea317416599d856929d
Abdelrahman Hany <98924552+AbdelrhmanAbuelnile@users.noreply.github.com>
2024-06-25 22:29:25 +0300

    DB connected successfuly

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('manga');
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const cors = require('cors')
+require('./db')
+
+app.use(cors())
+
+app.use((err,req,res,next)=>{
+	const statusCode = err.statusCode || 500;
+	res.status(statusCode).send({
+		status:statusCode,
+		message: err?.message || 'internal server error',
+		errors: err?.errors || []
+	})
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
